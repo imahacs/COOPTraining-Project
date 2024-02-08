c08eb2f498cf8108cf5ae2a6a3edb8fb81f08509
MAHA <mem880.a@.com>
2024-02-08 21:15:07 +0300

    fixing error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: false,
+    appDir: true,
+  },
+  distDir: 'out',
+  output: 'export',
+  images: {
+    loader: 'custom',
+    loaderFile: './ImageLoader.js',
   },
 };
 
